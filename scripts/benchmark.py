bc25e9c9cb9eb94c2330014156b00f4843009d53
Andreas Ipp <andreas.ipp@gmail.com>
2019-04-18 00:13:51 +0200

    Scan through various parameters in benchmark. Omit initialization.

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -1,28 +1,19 @@
-########################################
-## Environment variables that can be set
 import os
-
-## Custom environment variables
-# os.environ["MY_NUMBA_TARGET"] = "cuda"    # Target GPU using CUDA
-# os.environ["MY_NUMBA_TARGET"] = "numba"     # Target CPU
-# os.environ["MY_NUMBA_TARGET"] = "python"  # Pure Python version
-
-# os.environ["GAUGE_GROUP"] = "su3"
-# os.environ["GAUGE_GROUP"] = "su2"
-########################################
-
-
 import time
 import datetime
+import importlib
 
 from curraun.numba_target import use_cuda, use_numba
 if use_cuda:
     from numba import cuda
 import curraun
 import curraun.core as core
-import curraun.mv as mv
-import curraun.initial as initial
+import curraun.su2
+import curraun.su3
 import numpy as np
+import numba
+
+MAX_TIMEOUT = 10
 
 """
     Standard parameters (can be overwritten via passing arguments)
@@ -45,49 +36,100 @@ p = {
 
 # derived parameters
 a = p['L'] / p['N']
-E0 = p['N'] / p['L'] * 0.197326
 DT = 1.0 / p['DTS']
 maxt = int(p['TMAX'] / a) * p['DTS']
 
-np.random.seed(1)
+def time_simulation():
+    # Reload libraries
+    importlib.reload(curraun.numba_target)
+    importlib.reload(curraun)
+    importlib.reload(curraun.core)
+    importlib.reload(curraun.su2)
+    importlib.reload(curraun.su3)
+    importlib.reload(curraun.su)
+    importlib.reload(curraun.leapfrog)
+    importlib.reload(curraun.lattice)
 
-# initialization
-s = curraun.core.Simulation(p['N'], DT, p['G'])
-va = curraun.mv.wilson(s, mu=p['MU'] / E0, m=p['M'] / E0, uv=p['UV'] / E0, num_sheets=p['NS'])
-vb = curraun.mv.wilson(s, mu=p['MU'] / E0, m=p['M'] / E0, uv=p['UV'] / E0, num_sheets=p['NS'])
-curraun.initial.init(s, va, vb)
-
-print("Memory of data: {} GB".format(s.get_ngb()))
-
-if use_cuda:
-    s.copy_to_device()
-
-    meminfo = cuda.current_context().get_memory_info()
-    print("CUDA free memory: {:.2f} GB of {:.2f} GB.".format(meminfo[0] / 1024 ** 3, meminfo[1] / 1024 ** 3))
-
-# Evolve once for Just-In-Time compilation
-curraun.core.evolve_leapfrog(s)
-
-init_time = time.time()
-
-for t in range(maxt):
-    curraun.core.evolve_leapfrog(s)
+    from curraun.numba_target import use_cuda, use_numba
     if use_cuda:
+        from numba import cuda
+
+    np.random.seed(1)
+
+    # initialization
+    s = curraun.core.Simulation(p['N'], DT, p['G'])
+
+    print("Memory of data: {} GB".format(s.get_ngb()))
+
+    if use_cuda:
+        s.copy_to_device()
+
+        meminfo = cuda.current_context().get_memory_info()
+        print("CUDA free memory: {:.2f} GB of {:.2f} GB.".format(meminfo[0] / 1024 ** 3, meminfo[1] / 1024 ** 3))
+
+    if not use_numba:
+        # Evolve once for Just-In-Time compilation
+        curraun.core.evolve_leapfrog(s)
+
+    init_time = time.time()
+
+    for t in range(maxt):
+        curraun.core.evolve_leapfrog(s)
+        if use_cuda:
+            cuda.synchronize()
+        total_time = time.time() - init_time
+        if total_time > MAX_TIMEOUT:
+            break
+
+    if use_cuda:
+        # s.copy_to_host()
         cuda.synchronize()
+
     total_time = time.time() - init_time
-    if total_time > 20:
-        break
+    number_of_steps = t + 1
+    seconds_per_step = total_time / number_of_steps
+    estimated_total_time = seconds_per_step * maxt * p['NE']
+    estimated_total_time_formatted = str(datetime.timedelta(seconds=estimated_total_time))
 
-if use_cuda:
-    # s.copy_to_host()
-    cuda.synchronize()
+    print("Number of steps calculated: {}".format(number_of_steps))
+    print("Total time: {}".format(total_time))
+    print("Seconds per simulation step: {}".format(seconds_per_step))
+    print("Estimated total time: {}".format(estimated_total_time_formatted))
+    print("---------------------------------------")
 
-total_time = time.time() - init_time
-seconds_per_step = total_time / t
-estimated_total_time = seconds_per_step * maxt * p['NE']
-estimated_total_time_formatted = str(datetime.timedelta(seconds=estimated_total_time))
+def time_python_numba_cuda():
+    #os.environ["MY_NUMBA_TARGET"] = "python"
+    #time_simulation()
 
-print("Number of steps calculated: {}".format(t))
-print("Total time: {}".format(total_time))
-print("Seconds per simulation step: {}".format(seconds_per_step))
-print("Estimated total time: {}".format(estimated_total_time_formatted))
+    print("Number of threads: 1")
+    os.environ["NUMBA_NUM_THREADS"] = "1"
+    os.environ["MY_NUMBA_TARGET"] = "numba"
+    time_simulation()
+
+    del os.environ["NUMBA_NUM_THREADS"]
+    print("Number of threads: {}".format(numba.config.NUMBA_DEFAULT_NUM_THREADS))
+    os.environ["MY_NUMBA_TARGET"] = "numba"
+    time_simulation()
+
+    os.environ["MY_NUMBA_TARGET"] = "cuda"
+    time_simulation()
+
+print("---------------------------------------")
+
+# SU(2) Double
+os.environ["GAUGE_GROUP"] = "su2"
+os.environ["PRECISION"] = "double"
+time_python_numba_cuda()
+
+# SU(2) Single
+os.environ["PRECISION"] = "single"
+time_python_numba_cuda()
+
+# SU(3) Double
+os.environ["GAUGE_GROUP"] = "su3"
+os.environ["PRECISION"] = "double"
+time_python_numba_cuda()
+
+# SU(3) Single
+os.environ["PRECISION"] = "single"
+time_python_numba_cuda()
