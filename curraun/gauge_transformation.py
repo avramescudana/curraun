7a8358c77beb935d112cfda7d442b8ed47f4e6e3
Carlos Lamas Rodríguez <carlos.lamas.rodriguez@rai.usc.es>
2024-04-17 11:18:46 +0200

    Problems when removing the extra i*g*a

--- curraun/gauge_transformation.py
+++ curraun/gauge_transformation.py
@@ -9,24 +9,21 @@ import numpy as np
 import curraun.su as su
 
 # Constructs the plus links in temporal gauge over the x^+ axis
-@myjit
-def get_plus_links(ux, ae, z, g, a, N):
+def get_plus_links(ux, ae, z, N):
 # def get_plus_links(ux, ae, z, N):
-    r = su.mul(su.mexp(su.mul_s(ae, 1j*g*a*(z-N//2))), ux)
+    r = su.mul(su.mexp(su.mul_s(ae, (z-N//2))), ux)
     # r = su.mul(su.mexp(su.mul_s(ae, z-N//2)), ux)
     return r
 
 # Defines the gauge operator for a given step given its value at the previous one
-@myjit
-def gauge_transformation_operator(ux, ae, v, z, g, a, N):
+def gauge_transformation_operator(ux, ae, v, z, N):
 # def gauge_transformation_operator(ux, ae, v, z, N):
-    umin = su.mul(su.mexp(su.mul_s(ae, 1j*g*a*(z-N//2))), su.dagger(ux))
+    umin = su.mul(su.mexp(su.mul_s(ae, (z-N//2))), su.dagger(ux))
     # umin = su.mul(su.mexp(su.mul_s(ae, z-N//2)), su.dagger(ux))
     r = su.mul(umin, v)
     return r
 
 # Defines the gauge operator for a given step given its value at the previous one
-@myjit
 def act_on_links(u, v1, v2):
     r = su.mul(su.mul(su.dagger(v1), u), v2)
     return r
