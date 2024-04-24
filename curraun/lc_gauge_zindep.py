22ea8e2567ef432f9997877a6500c467c0c67b9f
Dana Avramescu <avramescudana@protonmail.com>
2024-04-24 23:01:08 +0300

    Work on GPU vs CPU debug

--- curraun/lc_gauge_zindep.py
+++ curraun/lc_gauge_zindep.py
@@ -82,6 +82,7 @@ class LCGaugeTransf:
         n = self.s.n
         nplus = self.nplus
         my_parallel_loop(init_vlc_kernel, n*nplus, self.vlc0, self.vlc1)
+        # my_parallel_loop(init_vlc_kernel, n*nplus, self.vlc1)
 
     def init(self):
         if use_cuda:
@@ -93,30 +94,21 @@ class LCGaugeTransf:
         if not self.initialized:
             self.init()
 
-        tint = round(self.s.t / self.s.dt)
-        tmoddts = tint//self.dts
-
         # Or equivalently if tint % self.dts == 0
         if self.s.t % self.s.dt == 0:
 
-            # if xplus > tint: 
-            if xplus > tmoddts:
-                # compute_vlc(self.d_vlc1, xplus, tint, self.s.n, self.nplus, self.s.d_u1, self.s.d_aeta1)
-                compute_vlc(self.d_vlc1, xplus, tint, self.s.n, self.nplus, self.s.d_u1)
+            # the previous vlc0 becomes the current vlc1, for the next xplus time step
+            # self.d_vlc0 = self.d_vlc1
+            # self.vlc0 = self.vlc1
 
-                # the previous vlc0 becomes the current vlc1, for the next xplus time step
-                self.d_vlc0 = self.d_vlc1
-                self.vlc0 = self.vlc1
+            compute_vlc(self.d_vlc0, self.d_vlc1, xplus, self.s.n, self.nplus, self.s.d_u1)
 
-            # elif xplus == tint and xplus != 0:
-            elif xplus == tmoddts and xplus != 0:
-                # compute_uplus(self.d_up_temp, tint, self.s.n, self.s.d_u0, self.s.d_aeta0)
-                compute_uplus_temp(self.d_up_temp, xplus, self.s.n, self.s.d_u0)
-                act_vlc_uplus(self.s.n, xplus, self.nplus, self.d_up_lc, self.d_up_temp, self.d_vlc0, self.d_vlc1)
+            compute_uplus_temp(self.d_up_temp, xplus, self.s.n, self.s.d_u0)
+            act_vlc_uplus(self.s.n, xplus, self.d_up_lc, self.d_up_temp, self.d_vlc0, self.d_vlc1)
 
-                if self.LCDEBUG:
-                    compute_uminus_temp(self.d_um_temp, xplus, self.s.n, self.s.d_u0)
-                    act_vlc_uminus(self.s.n, xplus, self.nplus, self.d_um_lc, self.d_um_temp, self.d_vlc1)
+            if self.LCDEBUG:
+                compute_uminus_temp(self.d_um_temp, xplus, self.s.n, self.s.d_u0)
+                act_vlc_uminus(self.s.n, xplus, self.nplus, self.d_um_lc, self.d_um_temp, self.d_vlc1)
 
         if use_cuda:
             self.copy_to_host()
@@ -130,50 +122,44 @@ def init_vlc_kernel(yi, vlc0, vlc1):
     su.store(vlc0[yi], su.unit())
     su.store(vlc1[yi], su.unit())
 
-
 """
     Computes the infinitesimal gauge transformation V_LC. 
 """
 
-# def compute_vlc(vlc1, xplus, t, n, nplus, u1, aeta1):
-#     my_parallel_loop(compute_vlc_kernel, n*nplus, n, nplus, t, u1, aeta1, vlc1, xplus)  
-
-def compute_vlc(vlc1, xplus, t, n, nplus, u1):
-    my_parallel_loop(compute_vlc_kernel, n*nplus, n, nplus, t, u1,  vlc1, xplus)  
+def compute_vlc(vlc0, vlc1, t, n, nplus, u1):
+    my_parallel_loop(compute_vlc_kernel, n*nplus, n, t, u1, vlc0, vlc1)  
 
 @myjit
-# def compute_vlc_kernel(yi, n, nplus, t, u1, aeta1, vlc1, xplus):
-def compute_vlc_kernel(yi, n, nplus, t, u1, vlc1, xplus):
-    xplusy = l.get_point_nxm(yi, nplus, n)
+def compute_vlc_kernel(yi, n, t, u1, vlc0, vlc1):
+
+    xplusy = l.get_point(yi, n)
     xplus, y = xplusy[0], xplusy[1]
 
-    xy_latt = l.get_index_nm(xplus+xplus-t, y, n)
-    ux_latt = u1[xy_latt, 0, :]
-    ux_dag = su.dagger(ux_latt)
+    if xplus > t and xplus!=0:
 
-    # aeta_latt = aeta1[xy_latt, :]
-    # aez = su.mul_s(aeta_latt, yz[1]-n//2)
-    # ae_exp = su.mexp(aez)
-    # umin = su.mul(ae_exp, ux_dag)
-    # res = su.mul(umin, vlc1[zi])
+        xy_latt = l.get_index_nm(xplus+xplus-t, y, n)
+        ux_latt = u1[xy_latt, 0, :]
+        ux_dag = su.dagger(ux_latt)
 
-    res = su.mul(ux_dag, vlc1[yi])
-    
-    su.store(vlc1[yi], res)
+        # aeta_latt = aeta1[xy_latt, :]
+        # aez = su.mul_s(aeta_latt, yz[1]-n//2)
+        # ae_exp = su.mexp(aez)
+        # umin = su.mul(ae_exp, ux_dag)
+        # res = su.mul(umin, vlc1[zi])
+
+        res = su.mul(ux_dag, vlc0[yi])
+        
+        su.store(vlc1[yi], res)
 
 
 """
     Extracts the value of U_+ along the x^+ axis.
 """
 
-# def compute_uplus(up_temp, t, n, u0, aeta0):
-#     my_parallel_loop(compute_uplus_kernel, n, t, n, u0, aeta0, up_temp)  
-
 def compute_uplus_temp(up_temp, t, n, u0):
     my_parallel_loop(compute_uplus_temp_kernel, n, t, n, u0, up_temp)  
 
 @myjit
-# def compute_uplus_kernel(yi, t, n, u0, aeta0, up_temp):
 def compute_uplus_temp_kernel(yi, t, n, u0, up_temp):
     ty_latt = l.get_index_nm(t, yi, n)
 
@@ -192,14 +178,13 @@ def compute_uplus_temp_kernel(yi, t, n, u0, up_temp):
     Aplies the gauge transformation V_LC on the U_+ gauge link.
 """
 
-def act_vlc_uplus(n, xplus, nplus, up_lc, up_temp, vlc0, vlc1):
-    my_parallel_loop(act_vlc_uplus_kernel, n, xplus, nplus, up_lc, up_temp, vlc0, vlc1)  
+def act_vlc_uplus(n, xplus, up_lc, up_temp, vlc0, vlc1):
+    my_parallel_loop(act_vlc_uplus_kernel, n, xplus, n, up_lc, up_temp, vlc0, vlc1)  
 
 @myjit
-def act_vlc_uplus_kernel(yi, xplus, nplus, up_lc, up_temp, vlc0, vlc1):
-    xplusy_latt = l.get_index_nm(xplus, yi, nplus)
+def act_vlc_uplus_kernel(yi, xplus, n, up_lc, up_temp, vlc0, vlc1):
+    xplusy_latt = l.get_index_nm(xplus, yi, n)
 
-    #TODO: Why this order?
     buff0 = su.dagger(vlc1[xplusy_latt])
     buff1 = su.mul(buff0, up_temp[yi])
     buff2 = su.mul(buff1, vlc0[xplusy_latt])
@@ -237,7 +222,6 @@ def act_vlc_uminus_kernel(yi, xplus, nplus, um_lc, um_temp, vlc1):
     # Gauge operator at x^- + delta x^-
     vlc2 = su.mul(um_temp[yi], vlc1[xplusy_latt])
 
-    #TODO: Why this order?
     buff0 = su.dagger(vlc2)
     buff1 = su.mul(buff0, um_temp[yi])
     buff2 = su.mul(buff1, vlc1[xplusy_latt])
