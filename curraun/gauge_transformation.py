826fddd78e2c4afb75a6c0bb08a50e5a63a2a68a
Carlos Lamas Rodríguez <carlos.lamas.rodriguez@rai.usc.es>
2024-09-06 16:07:29 +0200

    Corrected intitialization of the gauge transformation

--- curraun/gauge_transformation.py
+++ curraun/gauge_transformation.py
@@ -10,22 +10,27 @@ import curraun.su as su
 
 # Constructs the plus links in temporal gauge over the x^+ axis
 def get_plus_links(ux, ae, z, N, t):
-    if (z-N//2) > t or (z-N//2) < -t:
-        r = su.unit()
-    else:
+    if (z-N//2) <= t and (z-N//2) >= -t:
         r = su.mul(su.mexp(su.mul_s(ae, (z-N//2)/t**2)), ux)
+    else:
+        r = su.unit()
     return r
 
 # Defines the gauge operator for a given step given its value at the previous one
 def gauge_transformation_operator(ux, ae, v, z, N, t):
-    if (z-N//2) > t or (z-N//2) < -t:
-        r = su.unit()
-    else:
+    if (z-N//2) <= t and (z-N//2) >= -t:
         umin = su.mul(su.mexp(su.mul_s(ae, (z-N//2)/t**2)), ux)
         r = su.mul(su.dagger(umin), v)
+    else:
+        r = su.unit()
     return r
 
 # Defines the gauge operator for a given step given its value at the previous one
 def act_on_links(u_temp, v1, v2):
     r = su.mul(su.mul(su.dagger(v1), u_temp), v2)
     return r
+
+# Just to debug
+def getz(z, t, N):
+    if (z-N//2) <= t and (z-N//2) >= -t:
+        print(z-N//2)
