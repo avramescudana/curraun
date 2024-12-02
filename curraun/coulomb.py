f583dd5b38784fd0667add9f6e04fb210c159208
Dana Avramescu <avramescudana@protonmail.com>
2024-12-02 12:13:13 +0200

    Cleanup Coulomb gauge transformation, stable version with fourier acceleration

--- curraun/coulomb.py
+++ curraun/coulomb.py
@@ -13,6 +13,7 @@ from scipy.stats import unitary_group
     This is done at a fixed \tau time step and at a certain iteration.
     The only external parameter is the \alpha convergence parameter.
     #TODO: perform the guage transformation iterations on GPU
+    #TODO: iterate until convergence is reached
 """
 
 # Use cupy only if cuda is available
@@ -35,34 +36,24 @@ class CoulombGaugeTransf:
 
         # gauge transformation at previous iteration
         self.g0 = np.zeros((nn, su.GROUP_ELEMENTS), dtype=su.GROUP_TYPE)
-
         # gauge transformation at current iteration
         self.g1 = np.zeros((nn, su.GROUP_ELEMENTS), dtype=su.GROUP_TYPE)
 
-        # check unitarity of the gauge transformation
-        self.gunit = np.zeros(nn, dtype=su.GROUP_TYPE_REAL)
-
         # gauge links at previous iteration
         self.ug0 = np.zeros((nn, 2, su.GROUP_ELEMENTS), dtype=su.GROUP_TYPE)
-
         # gauge links at current iteration
         self.ug1 = np.zeros((nn, 2, su.GROUP_ELEMENTS), dtype=su.GROUP_TYPE)
 
+        #TODO: move this to separate debug function
+        # check unitarity of the gauge transformation
+        self.gunit = np.zeros(nn, dtype=su.GROUP_TYPE_REAL)
         # check unitarity of the gauge links
         self.ugunit = np.zeros((nn, 2), dtype=su.GROUP_TYPE_REAL)
 
         # divergence of the gauge field at previous iteration
         self.delta = np.zeros((nn, su.GROUP_ELEMENTS), dtype=su.GROUP_TYPE)
-
-        # divergence of the gauge field at current iteration
-        # self.delta1 = np.zeros((nn, su.GROUP_ELEMENTS), dtype=su.GROUP_TYPE)
-
         # foruier accelerated coulomb gauge condition
         self.c = np.zeros((nn, su.GROUP_ELEMENTS), dtype=su.GROUP_TYPE)
-
-        #TODO: algebra color elements debugging
-        self.delta_alg = np.zeros((nn, su.ALGEBRA_ELEMENTS), dtype=su.GROUP_TYPE_REAL)
-        self.c_alg = np.zeros((nn, su.ALGEBRA_ELEMENTS), dtype=su.GROUP_TYPE_REAL)
 
         # convergence criterion
         self.thetax = np.zeros(nn, dtype=su.GROUP_TYPE)
@@ -74,16 +65,13 @@ class CoulombGaugeTransf:
         self.d_ug0 = self.ug0
         self.d_ug1 = self.ug1
         self.d_delta = self.delta
-        # self.d_delta1 = self.delta1
         self.d_theta = self.theta
         self.d_c = self.c
         self.d_thetax = self.thetax
+
+        #TODO: move this to separate debug function
         self.d_ugunit = self.ugunit
         self.d_gunit = self.gunit
-
-        #TODO: algebra color elements debugging
-        self.d_delta_alg = self.delta_alg
-        self.d_c_alg = self.c_alg
 
     def copy_to_device(self):
         self.d_g0 = cuda.to_device(self.g0)
@@ -91,16 +79,13 @@ class CoulombGaugeTransf:
         self.d_ug0 = cuda.to_device(self.ug0)
         self.d_ug1 = cuda.to_device(self.ug1)
         self.d_delta = cuda.to_device(self.delta)
-        # self.d_delta1 = cuda.to_device(self.delta1)
         self.d_theta = cuda.to_device(self.theta)
         self.d_c = cuda.to_device(self.c)
         self.d_thetax = cuda.to_device(self.thetax)
+
+        #TODO: move this to separate debug function
         self.d_ugunit = cuda.to_device(self.ugunit)
         self.d_gunit = cuda.to_device(self.gunit)
-
-        #TODO: algebra color elements debugging
-        self.d_delta_alg = cuda.to_device(self.delta_alg)
-        self.d_c_alg = cuda.to_device(self.c_alg)
 
     def copy_to_host(self):
         self.d_g0.copy_to_host(self.g0)
@@ -108,16 +93,13 @@ class CoulombGaugeTransf:
         self.d_ug0.copy_to_host(self.ug0)   
         self.d_ug1.copy_to_host(self.ug1)
         self.d_delta.copy_to_host(self.delta)
-        # self.d_delta1.copy_to_host(self.delta1)
         self.d_theta.copy_to_host(self.theta)
         self.d_c.copy_to_host(self.c)
         self.d_thetax.copy_to_host(self.thetax)
+
+        #TODO: move this to separate debug function
         self.d_ugunit.copy_to_host(self.ugunit)
         self.d_gunit.copy_to_host(self.gunit)
-
-        #TODO: algebra color elements debugging
-        self.d_delta_alg.copy_to_host(self.delta_alg)
-        self.d_c_alg.copy_to_host(self.c_alg)
 
     def copy_theta_to_device(self, stream=None):
         self.d_theta = cuda.to_device(self.p_theta, stream)
@@ -131,8 +113,7 @@ def init_gauge_transform(self):
 
     # initialize gauge transformation with identity matrix
     init_transf(self.s, self.d_g0)
-    # init_transf_random(self.s, self.d_g0)
-
+   
     # initialize gauge links with the glasma ones
     my_parallel_loop(init_gauge_links_kernel, nn, self.s.d_u0, self.d_ug0)
 
@@ -173,30 +154,18 @@ def gauge_transform(self, alpha):
     # check_ugunit(self.s, self.d_ugunit, self.d_ug1)
 
     # compute delta with previous iteration
-    # compute_delta(self.s, self.d_g0, self.d_ug1, self.d_delta)
     compute_delta(self.s, self.d_ug1, self.d_delta)
 
     compute_thetax(self.s, self.d_delta, self.d_thetax)
     self.d_theta = cupy.mean(cupy.array(self.d_thetax))
 
-    #TODO: algebra color elements debugging
-    # compute_delta_alg(self.s, self.d_ug1, self.d_delta_alg)
-    # compute_thetax_alg(self.s, self.d_delta_alg, self.d_thetax)
-
     self.theta = np.mean(self.d_thetax).real
-    # self.theta = np.mean(self.d_thetax[1])
 
     # apply fourier acceleration
-    # fourier_acceleration(self.s, self.d_delta, alpha, self.d_c)
+    fourier_acceleration(self.s, self.d_delta, self.d_c)
 
     # update the gauge transformation
-    # update_gauge_transf(self.s, self.d_g0, self.d_c, self.d_g1)
-
-    #TODO: remove this, just debugging without fourier acceleration
-    update_gauge_transf(self.s, self.d_g0, self.d_delta, self.d_g1)
-
-    #TODO: algebra color elements debugging
-    # update_gauge_transf_alg(self.s, self.d_g0, self.d_delta_alg, self.d_g1)
+    update_gauge_transf(self.s, self.d_g0, self.d_c, self.d_g1, alpha)
 
     # check unitarity of the gauge transformation
     #TODO: separate the unitarity check as debugging
@@ -241,30 +210,16 @@ def gauge_transf_links_kernel(xi, n, g1, ug0, ug1):
         xiplus = l.shift(xi, d, +1, n)
         ug1[xi, d] = l.dact(g1[xi], g1[xiplus], ug0[xi, d])
 
-def update_gauge_transf(s, g0, c, g1):
+def update_gauge_transf(s, g0, c, g1, alpha):
     n = s.n
     nn = n ** 2
 
-    my_parallel_loop(update_gauge_transf_kernel, nn, n, g0, c, g1)
+    my_parallel_loop(update_gauge_transf_kernel, nn, n, g0, c, g1, alpha)
 
 @myjit
-def update_gauge_transf_kernel(xi, n, g0, c, g1):
-    # buf = su.mexp(c[xi])
-    buf = su.mul_s(su.mexp(c[xi]), -1/(4*su.NC*n**2))
+def update_gauge_transf_kernel(xi, n, g0, c, g1, alpha):
+    buf = su.mul_s(su.mexp(c[xi]), alpha)
     g1[xi] = su.mul(buf, g0[xi])
-
-#TODO: algebra color elements debugging
-def update_gauge_transf_alg(s, g0, c, g1):
-    n = s.n
-    nn = n ** 2
-
-    my_parallel_loop(update_gauge_transf_alg_kernel, nn, g0, c, g1)
-
-@myjit
-def update_gauge_transf_alg_kernel(xi, g0, c, g1):
-    buf1 = su.get_algebra_element(c[xi])
-    buf2 = su.mexp(buf1)
-    g1[xi] = su.mul(buf2, g0[xi])
 
 def compute_thetax(s, delta, thetax):
     n = s.n
@@ -276,123 +231,55 @@ def compute_thetax(s, delta, thetax):
 def compute_thetax_kernel(xi, delta, thetax):
     thetax[xi] = su.tr(su.mul(delta[xi], su.dagger(delta[xi])))
 
-#TODO: algebra color elements debugging
-def compute_thetax_alg(s, delta, thetax):
-    n = s.n
-    nn = n ** 2 
-
-    my_parallel_loop(compute_thetax_alg_kernel, nn, delta, thetax)
-
-@myjit
-def compute_thetax_alg_kernel(xi, delta, thetax):
-    buf = su.get_algebra_element(delta[xi])
-    thetax[xi] = su.tr(su.mul(buf, su.dagger(buf)))
-
-
-# def compute_delta(s, g0, ug0, delta):
 def compute_delta(s, ug0, delta):
-    # u0 = s.d_u0
-    # u1 = s.d_u1
-
     n = s.n
 
-    # my_parallel_loop(compute_delta_kernel, n * n, n, u0, ug0, g0, delta)
     my_parallel_loop(compute_delta_kernel, n * n, n, ug0, delta)
 
 @myjit
-# def compute_delta_kernel(xi, n, u0, ug0, g0, delta):
 def compute_delta_kernel(xi, n, ug0, delta):
     # Delta = \sum_i [(U_x-i,i - U_x,i) - hc - trace] with i=x,y 
-
     buf = su.zero()
 
     for d in range(2):
-        # xiplus = l.shift(xi, d, +1, n)
-        # ug0[xi, d] = l.dact(g0[xi], g0[xiplus], u0[xi, d])
-
         ximinus = l.shift(xi, d, -1, n)
-        # temp1 = l.add_mul(ug0[ximinus, d], ug0[xi, d], -1)
         temp1 = l.add_mul(ug0[xi, d], ug0[ximinus, d], -1)
         temp2 = su.dagger(temp1)
         temp3 = l.add_mul(temp1, temp2, -1)
-        # temp4 = su.mul_s(su.unit(), su.tr(temp3)/su.NC)
-        temp4 = su.mul_s(su.unit(), su.tr(temp3))
-
+        temp4 = su.mul_s(su.unit(), su.tr(temp3)/su.NC)
         temp5 = l.add_mul(temp3, temp4, -1)
         buf = su.add(buf, temp5)
 
     su.store(delta[xi], buf)
 
-#TODO: algebra color elements debugging
-def compute_delta_alg(s, ug0, delta):
+def fourier_acceleration(s, delta, c):
     n = s.n
-
-    my_parallel_loop(compute_delta_alg_kernel, n * n, n, ug0, delta)
-
-#TODO: algebra color elements debugging
-@myjit
-def compute_delta_alg_kernel(xi, n, ug0, delta):
-    ximinusx = l.shift(xi, 0, -1, n)
-    buf1x = l.add_mul(ug0[xi, 0], ug0[ximinusx, 0], -1)
-    buf2x = su.get_algebra_factors_from_group_element_approximate(buf1x)
-
-    ximinusy = l.shift(xi, 1, -1, n)
-    buf1y = l.add_mul(ug0[xi, 1], ug0[ximinusy, 1], -1)
-    buf2y = su.get_algebra_factors_from_group_element_approximate(buf1y)
-
-    buf = su.add_algebra(buf2x, buf2y)
-    delta[xi] = su.mul_algebra(buf, -0.5)
-
-def fourier_acceleration(s, delta, alpha, c):
-    n = s.n
-    # n_fft = (n // 2 + 1) if n % 2 == 0 else (n + 1) // 2
 
     # fourier transform to momentum space
     delta_reshape = cupy.reshape(cupy.array(delta), (n, n, su.GROUP_ELEMENTS))
-    # delta_fft = cupy.fft.rfft2(delta_reshape, axes=(0, 1))
     delta_fft = cupy.fft.fft2(delta_reshape, axes=(0, 1))
 
     # fourier accelerate with alpha
-    # delta_fft_reshape = cupy.reshape(delta_fft, (n*n_fft, su.GROUP_ELEMENTS))
     delta_fft_reshape = cupy.reshape(delta_fft, (n*n, su.GROUP_ELEMENTS))
 
-    # my_parallel_loop(fourier_acceleration_kernel, n , n, n_fft, cupy.asnumpy(delta_fft_reshape), alpha)
-    my_parallel_loop(complex_fourier_acceleration_kernel, n*n , n, cupy.asnumpy(delta_fft_reshape), alpha)
+    my_parallel_loop(complex_fourier_acceleration_kernel, n*n , n, cupy.asnumpy(delta_fft_reshape))
 
     # inverse fourier transform to position space
-    # delta_accfft_reshape = cupy.reshape(cupy.array(delta_fft_reshape), (n, n_fft, su.GROUP_ELEMENTS))
     delta_accfft_reshape = cupy.reshape(cupy.array(delta_fft_reshape), (n, n, su.GROUP_ELEMENTS))
 
-    # delta_accfft = cupy.fft.irfft2(delta_accfft_reshape, axes=(0, 1), s=(n, n))
     delta_accfft = cupy.fft.ifft2(delta_accfft_reshape, axes=(0, 1), s=(n, n))
-    
     c_fft = cupy.asnumpy(cupy.reshape(delta_accfft, (n * n, su.GROUP_ELEMENTS)))
-
     my_parallel_loop(store_c_fft_kernel, n*n , c_fft, c)
 
 @myjit  
-def fourier_acceleration_kernel(x, n, n_fft, delta_fft, alpha):
-    for y in range(n_fft):
-        if (x > 0 or y > 0):
-            # extract p^2 and p^2_max
-            psq = psq_latt(x, y, n)
-            # psqmax = (2*np.pi/n)**2    
-            psqmax = psq_latt(n-1, n-1, n)
-
-            buf = su.mul_s(delta_fft[x], alpha * psqmax / psq)
-            # buf = su.mul_s(delta_fft[x], alpha / psq)
-            su.store(delta_fft[x], buf)
-
-@myjit  
-def complex_fourier_acceleration_kernel(xi, n, delta_fft, alpha):
+def complex_fourier_acceleration_kernel(xi, n, delta_fft):
     x, y = l.get_point(xi, n)
 
     # extract p^2 and p^2_max
     psq = psq_latt(x, y, n)
-    # psqmax = (2*np.pi/n)**2    
     psqmax = psq_latt(n-1, n-1, n)
 
-    buf = su.mul_s(delta_fft[xi], alpha * psqmax / psq)
+    buf = su.mul_s(delta_fft[xi], psqmax / psq)
     su.store(delta_fft[xi], buf)
 
 @myjit
