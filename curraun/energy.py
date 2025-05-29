300e04270aae9e81cd819a6d1e4c8f06b1d1f465
Pooja <pooja.khowal45@gmail.com>
2025-05-30 00:54:52 +0300

    Checking if git push working

--- curraun/energy.py
+++ curraun/energy.py
@@ -58,6 +58,7 @@ class Energy():
         self.d_BL.copy_to_host(self.BL)
         self.d_ET.copy_to_host(self.ET)
         self.d_BT.copy_to_host(self.BT)
+        
 
     def compute(self):
         # compute contributions in 2d
@@ -76,6 +77,7 @@ class Energy():
         BL = self.d_BL
         ET = self.d_ET
         BT = self.d_BT
+        
 
         n = self.s.n
 
